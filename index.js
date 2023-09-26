29cea76ae2e6f6ce34d4d3bc87b31f87eb13c5e9
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-26 11:40:15 +0300

    complete

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(d){
   // Write your code here!
+  if(d<=400) {return 'This one is on me!'}
+  else if(d >2000 & d <=2500) {return 'I will gladly take your thirty bucks.'}
+  else if(d >2500){return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(n){
   // Write your code here!
+if(n==="NYC") {return "Ok, sounds good."}
+else if(n==="Pittsburgh") {return 'No go.'}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(g){
   // Write your code here!
+  if(g==='generous') {return'Thank you so much.'}
+  else if(g==='not as generous') {return'Thank you.'}
+  else if(g==='thanks for everything') {return 'Bye.'}
 }
